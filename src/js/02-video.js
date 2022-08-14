b2e88bc5fab3547d5246350fb3435bb203b7d659
TetianaPiatykh <tanya.pjatykh@gmail.com>
2022-08-14 15:16:24 +0300

    hw-08-4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+     localStorage.setItem("videoplayer-current-time", evt.seconds);
+    }
+
+
+let currentTime = localStorage.getItem("videoplayer-current-time");
+console.log(currentTime);
+if (currentTime) {
+player.setCurrentTime(currentTime);
+} 
+
+
+    // .then(function (seconds) {
+//     seconds = currentTime.seconds;
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
