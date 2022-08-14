3cded71186b2a67e3c0f1510061ff07d56b36e1b
TetianaPiatykh <tanya.pjatykh@gmail.com>
2022-08-14 12:42:03 +0300

    hw-08-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
+
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+    <a class="gallery__link"
+    href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    alt="${description}" />
+    </a>
+    </li>
+        `;
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+galleryContainer.addEventListener('click', lightbox)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+
+
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+populatedTextarea();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(event) {
+    formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populatedTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        form.message.value = savedMessage.message || '';
+        form.email.value = savedMessage.email || '';
+    }
+    // console.log(savedMessage);
+}
 
